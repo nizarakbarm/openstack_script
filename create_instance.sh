02aea942856051c3971d403805f28fdce673a18f
nizarakbarm <nizarakbarm04@gmail.com>
2018-12-14 02:44:41 +0700

    add function create fixed ip and create floating ip

--- create_instance.sh
+++ create_instance.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #help message
 usage() {
-   echo "usage: $0 -ins|--instance node1,node2,noden -img|--image imagetype -flv|--flavor flavortype"
+   echo -e "usage: $0 \n \
+   options: \n \
+   -ins|--instance node1,node2,nodeN -img|--image imagetype -flv|--flavor flavortype \n \
+   [-ip|--fixed-ip network=networkname,ip=ipaddress,name=fixedipname] \n \
+   [--create-floating]"
 }
+
 #read pod name
 readPod() {
    local arg=$1
@@ -12,22 +17,46 @@ readPod() {
 isPortIPFloat() {
    openstack floating ip list --port $1 | grep "10.1" | cut -d'|' -f3
 }
+
+#create fixed ip address
+createFixedIp() {
+	local netarg=($(echo $1 | tr ',' '\n'))
+
+	network=$(cut -d"=" -f2 <<< "${netarg[0]}")
+	fixip=$(cut -d"=" -f2 <<< "${netarg[1]}")
+	ipname=$(cut -d"=" -f2 <<< "${netarg[2]}")
+
+	openstack port create --network "$network" --fixed-ip ip-address="$fixip" "$ipname"
+}
+
+createFloating() {
+	openstack floating ip create ext-net
+}
+
 #mapping ip floating to pod
 mapFloating() {
    if [ -z $(isPortIPFloat $1) ]; then
    	openstack floating ip set --port=$1 $2
    fi
 }
+
+
 #create instance
 #instances=(pod43-node1 pod43-node2 pod43-node3)
 createInstance() {
 local IFS=$'\n'
-local ports=($(openstack port list | grep port | sort -k3 | cut -f2 -d'|' | tr -d [[:blank:]]))
-local floats=($(openstack floating ip list | grep "10." | cut -d'|' -f3 | tr -d [[:blank:]]))
+local floats=($(openstack floating ip list | grep "None" | sort -k2 | cut -d'|' -f3 | tr -d [[:blank:]]))
 local i
 for ((i=0; i<${#instances[@]}; i++)); do
 nova boot --nic port-id="${ports[$i]}" --key-name nizar --image "$image" --flavor "$flavor" "${instances[$i]}"
 #openstack floating ip set --port="${ports[$i]}" "${floats[$i]}"  
+if [ "$ipname" == "" ]; then
+	local ports=($(openstack port list | grep DOWN | sort -k3 | cut -f2 -d'|' | tr -d [[:blank:]]))
+	mapFloating "${ports[$i]}" "${floats[$i]}"
+else
+	mapFloating  "$name" "${floats[$i]}"
+fi
+
 mapFloating "${ports[$i]}" "${floats[$i]}"
 done
 }
@@ -51,6 +80,12 @@ while (( $# )); do
       -flv | --flavor) flavor="$2"
                       shift 2
 		      ;;
+      -ip | --fixed-ip) createFixedIp $2
+	             shift 2
+		     ;;
+      --create-floating) createFloating
+	            shift 2
+		    ;;
       -h | --help)     usage
 		       shift
                        exit
